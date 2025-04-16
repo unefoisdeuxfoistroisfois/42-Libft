1ad0c46029048d2c11089f179271931b21990773
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2025-04-16 17:48:31 +0200

    Ajout de la fonction memmove

---
+++ ft_memmove.c
@@ -0,0 +1,56 @@
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: britela- <britela-@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/16 13:32:23 by britela-          #+#    #+#             */
+/*   Updated: 2025/04/16 14:25:08 by britela-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char	*dest2;
+	unsigned char	*src2;
+	size_t	i;
+	if(src == NULL && dest == NULL)
+	{
+		return (NULL);
+	}
+	dest2 = (unsigned char	*)dest;
+	src2 = (unsigned char	*)src;
+	i = 0;
+
+	if (dest < src)
+	{
+		while (n != 0)
+		{
+			dest2[i] = src2[i];
+			i++;
+			n--;
+		}
+	}
+	else
+	{
+		while (n != 0)
+		{
+			n--;
+			dest2[n] =  src2[n];
+		}
+	}
+	return (dest);
+}
+/*
+int	main()
+{
+	//char	mot[] = "Salut";
+	//char	mot2[] = "Bradley";
+	char	mot[] = "SalutBradley";
+
+	// chevauchemetn ? quand une chaine se partage la meme memoire 
+	ft_memmove(mot+2, mot, 5);
+	return (0);
+}*/
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,27 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: britela- <britela-@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/14 14:45:54 by britela-          #+#    #+#             */
-/*   Updated: 2025/04/16 13:40:32 by britela-         ###   ########.fr       */
+/*   Created: 2025/04/16 16:24:39 by britela-          #+#    #+#             */
+/*   Updated: 2025/04/16 17:47:34 by britela-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+size_t	strlcpy(char *dsst, const char *src, size_t dstsize)
+{
+	
+}
 
-#include <stdlib.h>
-#include <stdio.h>
-
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-size_t	ft_strlen(const char *str);
-void	*ft_memset(void *s, int c, size_t n);
-void	ft_bzero(void *s, size_t n);
-#endif
+int	main()
+{
+	return(0);
+}
