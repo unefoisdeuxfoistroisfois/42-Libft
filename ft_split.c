3a6b6124623d2a9537a89cd7d7b6f3f474b7e9f0
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2025-04-23 23:50:13 +0200

    ENFINT FINI SPLIT IL ETAIT TROP DUR

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: britela- <britela-@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 22:02:24 by britela-          #+#    #+#             */
-/*   Updated: 2025/04/23 17:46:47 by britela-         ###   ########.fr       */
+/*   Updated: 2025/04/23 23:43:11 by britela-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,31 +31,75 @@ int	countword(char const *s, char c)
 	return (count);
 }
 
+char	*ft_strdup_range(const char *s, int start, int end)
+{
+	int		i;
+	char	*res;
+
+	res = malloc((end - start + 1) * sizeof(char));
+	if (res == NULL)
+		return (NULL);
+	i = 0;
+	while (start < end)
+	{
+		res[i] = s[start];
+		start++;
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
+}
+
 char	**ft_split(char	const *s, char c)
 {
+	int	i;
+	int	j;
+	int	start;
 	char	**newword;
 
 	if (s == NULL)
 	{
-		return (NULL)
+		return (NULL);
 	}
 
-	newword = malloc(sizeof(char) * (countword(s,c)) + 1);
+	newword = malloc(sizeof(char *) * (countword(s,c) + 1));
 	if (newword == NULL)
 	{
 		return (NULL);
 	}
+
+	i = 0;
+	j = 0;
+	while(s[i] != '\0')
+	{
+		if (s[i] != c && (i == 0 || s[i - 1] == c))
+			start = i;
+		if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
+		{
+			newword[j] = ft_strdup_range(s, start, i + 1);
+			j++;
+		}
+		i++;
+	}
+	newword[j] = NULL;
+	return (newword);
 }
 int	main()
 {
 	char	*mot;
 	char	sep;
-	int	res;
-
+	char	**res;
+	int	i;
 	mot = "Br.ad.l.ey";
 	sep = '.';
-	res = countword(mot, sep);
-	printf("%d",res);
+	res = ft_split(mot, sep);
+	
+	i = 0;
+	while (res[i] != NULL)
+	{
+		printf("mot[%d] = %s\n", i, res[i]);
+		i++;
+	}
 
 	return (0);
 }
