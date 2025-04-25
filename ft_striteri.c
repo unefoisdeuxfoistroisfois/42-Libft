9b48eee59318f35955dc14a4119c11bf0749a072
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2025-04-25 19:17:56 +0200

    Norminette pour ft_striteri et début de putchar_fd

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,22 +6,21 @@
 /*   By: britela- <britela-@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 16:08:30 by britela-          #+#    #+#             */
-/*   Updated: 2025/04/25 16:50:58 by britela-         ###   ########.fr       */
+/*   Updated: 2025/04/25 17:54:31 by britela-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "unistd.h"
 
-void	ft_striteri(char *s, void (*f)(unsigned int,char*))
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
 	unsigned int	i;
 
 	if (s == NULL && f == NULL)
 	{
-		return (NULL);
+		return ;
 	}
-
 	i = 0;
 	while (s[i] != '\0')
 	{
@@ -29,13 +28,13 @@ void	ft_striteri(char *s, void (*f)(unsigned int,char*))
 		i++;
 	}
 }
-
-void	ft_pointeurfonction(unisnged int i, char *s)
+/*
+void	ft_pointeurfonction(unsigned int i, char *c)
 {
 	char	maj;
 
 	if (i % 2 == 0 && *c >= 'a' && *c <= 'z')
-	}
+	{
 		maj = *c - 32;
 	}
 	else
@@ -45,12 +44,11 @@ void	ft_pointeurfonction(unisnged int i, char *s)
 	write(1,&maj,1);
 }
 
-}
 int	main()
 {
 	char	*mot;
 
 	mot = "Bradley";
-	ft_striteri(&mot,&ft_pointeurfonction);
+	ft_striteri(mot, &ft_pointeurfonction);
 	return (0);
-}
+}*/
